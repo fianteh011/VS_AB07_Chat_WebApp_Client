a1bcebcc664792ed7887ad8d65db87db310a88f5
fianteh110 <hafiyyan98@gmail.com>
2021-12-14 18:02:01 +0100

    Initial commit server running no implementations yet

---
+++ src/main/java/de/hsos/vs_ab07_chat_webapp_client/HelloApplication.java
@@ -0,0 +1,9 @@
+package de.hsos.vs_ab07_chat_webapp_client;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
